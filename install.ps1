1cc653a7c862bbd155464f60ba94306e4dbf678c
CRIT-Designs <cr.draftingservices@gmail.com>
2026-04-15 14:28:35 -0700

    Update install.ps1 as the deployed job was failing

--- install.ps1
+++ install.ps1
@@ -1,7 +1,7 @@
 Write-Host "Starting App Installation..."
 
-winget install --id Git.Git -e -silent
-winget install --id Microsoft.VisualStudioCode -e --silent
-winget install --id Google.Chrome -e --silent
+winget install --id Git.Git -e --silent --accept-package-agreements --accept-source-agreements
+winget install --id Microsoft.VisualStudioCode -e --silent --accept-package-agreements --accept-source-agreements
+winget install --id Google.Chrome -e --silent --accept-package-agreements --accept-source-agreements
 
-Write-Host "Installation Complete"
+Write-Host "Installation Complete!"
