56d6fdcfd147a3b5e3ae259ac3a5e9d6e3b06a1c
CRIT-Designs <cr.draftingservices@gmail.com>
2026-04-16 04:07:27 -0700

    Update install.ps1 "Test pipeline with VS Code install only"

--- install.ps1
+++ install.ps1
@@ -1,7 +1,8 @@
+$ErrorActionPreference = "Stop"
+
 Write-Host "Starting App Installation..."
 
-winget install --id Git.Git -e --silent --accept-package-agreements --accept-source-agreements
-winget install --id Microsoft.VisualStudioCode -e --silent --accept-package-agreements --accept-source-agreements
-winget install --id Google.Chrome -e --silent --accept-package-agreements --accept-source-agreements
+winget source update
+winget install --id Microsoft.VisualStudioCode -e --silent --source winget --accept-package-agreements --accept-source-agreements
 
 Write-Host "Installation Complete!"
